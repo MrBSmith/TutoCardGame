6de35e83c1acd3c0d7436d418e6bb9f1aea8406e
Hugo Baba <hugobarette@gmail.com>
2024-11-16 10:55:45 +0100

    Drag & drop

--- Scenes/card/card.gd
+++ Scenes/card/card.gd
@@ -1,20 +1,46 @@
 extends Control
 class_name Card
 
+enum STATE {
+	IN_HAND,
+	DRAG,
+	ON_BOARD,
+}
+
 @export_range(0.0, 200.0) var hovered_offset = 60.0
 @onready var pivot: Control = $Pivot
+@onready var panel : Panel = %Panel
+@export var state : STATE = STATE.IN_HAND
 
 var hovered := false
 
-
-func _process(delta: float) -> void:
-	var dest = -hovered_offset if hovered else 0.0
-	pivot.position.y = lerp(pivot.position.y, dest, 0.2)
-
-
-func _on_panel_mouse_entered() -> void:
-	mouse_entered.emit()
+func _ready() -> void:
+	panel.mouse_entered.connect(mouse_entered.emit)	
+	panel.mouse_exited.connect(mouse_exited.emit)	
+	panel.gui_input.connect(_on_Panel_gui_input)
 
 
-func _on_panel_gui_input(event: InputEvent) -> void:
+func _process(_delta: float) -> void:
+	match(state):
+		STATE.IN_HAND:
+			var dest = -hovered_offset if hovered else 0.0
+			pivot.position.y = lerp(pivot.position.y, dest, 0.2)
+		STATE.DRAG:
+			var mouse_pos = get_global_mouse_position()
+			pivot.position = Vector2.ZERO
+			global_position = mouse_pos
+
+
+func _on_Panel_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.is_pressed():
+			if state == STATE.IN_HAND:
+				state = STATE.DRAG
+		else:
+			if state == STATE.DRAG:
+				state = STATE.IN_HAND
+
+
+
+func _on_panel_gui_input(_event: InputEvent) -> void:
 	pass # Replace with function body.
--- Scenes/hand/hand.gd
+++ Scenes/hand/hand.gd
@@ -10,15 +10,18 @@ var hovered_card: Card = null:
 		if value != hovered_card:
 			hovered_card = value
 			hovered_card_changed.emit()
-			print("Hovered card: %s" % value.name)
 
 
 func _ready() -> void:
 	hovered_card_changed.connect(_update_card_hover)
+	var hover_fn = func(card: Card): hovered_card = card
+	var unhover_fn = func(card: Card): 
+		if hovered_card == card: 
+			hovered_card = null
 	
 	for card in cards_container.get_children():
-		var hover_fn = func(card: Card): hovered_card = card
 		card.mouse_entered.connect(hover_fn.bind(card))
+		card.mouse_exited.connect(unhover_fn.bind(card))
 
 
 func _process(_delta: float) -> void:
@@ -26,12 +29,15 @@ func _process(_delta: float) -> void:
 	
 	for i in range(cards.size()):
 		var card = cards[i]
+		
+		if card.state != Card.STATE.IN_HAND:
+			continue
+
 		var offset = cards_container.size.x / (cards.size() + 1)
 		card.position.x = lerp(card.position.x, offset * (i + 1), 0.3)
 		card.position.y = cards_container.size.y / 2.0
 
 
 func _update_card_hover() -> void:
-	print("update hovered")
 	for card in cards_container.get_children():
 		card.hovered = card == hovered_card
